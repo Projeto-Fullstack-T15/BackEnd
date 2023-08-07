f39602046df222e58f18291acfde9b30d018f142
Renan G. Dutra <r.granatodutra@gmail.com>
2023-08-07 20:23:41 -0300

    chore: change data-source schema

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,20 +1,23 @@
-import "reflect-metadata";
 import 'dotenv/config';
-import { DataSource } from "typeorm";
-import { User } from "./entity/User";
+import 'reflect-metadata';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as path from 'path';
 
-type DatabaseType = "mysql" | "postgres" | "sqlite";
+const dataSourceConfig = (): DataSourceOptions => {
+    const entitiesPath: string = path.join(__dirname, './entities/**.{ts,js}');
+    const migrationsPath: string = path.join(__dirname, './migrations/**.{ts,js}');
 
-export const AppDataSource = new DataSource({
-    type: process.env.DATABASE_TYPE as DatabaseType,
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-});
+    const { DB_TYPE, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+    const databaseURL = `${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+    return {
+        type: DB_TYPE as "postgres" | "mysql",
+        url: databaseURL,
+        synchronize: false,
+        logging: false,
+        entities: [entitiesPath],
+        migrations: [migrationsPath]
+    };
+};
+
+export const AppDataSource = new DataSource(dataSourceConfig());
