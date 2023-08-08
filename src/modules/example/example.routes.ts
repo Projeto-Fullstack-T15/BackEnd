ff6cd79b429ab984610c2d28103a0351cb1bfdd5
caio <chss.bolsa99@gmail.com>
2023-08-08 14:48:58 -0300

    feat schemas

--- src/modules/example/example.routes.ts
+++ src/modules/example/example.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import * as controllers from "./example.controllers";
 import { dataIsValidMidd } from "./example.middlewares";
-
+import { AnnouncementSchemaRequest } from "./example.schema";
 export const exampleRoutes = Router();
 
 exampleRoutes.post(
   "/announcement",
-  dataIsValidMidd,
+  dataIsValidMidd(AnnouncementSchemaRequest),
   controllers.getHelloController
 );
--- src/modules/example/example.services.ts
+++ src/modules/example/example.services.ts
@@ -1,11 +1,12 @@
-export async function getHelloService(): Promise<string> {
-    const randomTime = Math.random() * 1000;
+// import { AppDataSource } from "../../data-source";
+// import { Repository } from "typeorm";
+// import { Announcement } from "../../entity/Announcement";
+// export async function createannouncement(): Promise<any> {
+//   const movieRepository: Repository<Announcement> = AppDataSource.getRepository(Movie);
+//   const movie: Movie = movieRepository.create(movieData);
+//   await movieRepository.save(movie);
+//   const returnMovie: Tmovie = movieSchema.parse(movie);
+//   return returnMovie;
 
-    const promiseHelloWorld = new Promise<string>((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Hello World!");
-        }, randomTime);
-    });
-
-    return await promiseHelloWorld;
-} 
+//   return await promiseHelloWorld;
+// }
