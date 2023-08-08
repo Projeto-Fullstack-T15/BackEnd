3dca2be4145d2bfc01804c26bf45ab928e6e67ba
caio <chss.bolsa99@gmail.com>
2023-08-08 12:51:03 -0300

    feat midd schema

--- src/modules/example/example.routes.ts
+++ src/modules/example/example.routes.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import * as controllers from "./example.controllers";
+import { dataIsValidMidd } from "./example.middlewares";
 
 export const exampleRoutes = Router();
 
-exampleRoutes.post("hello-world/", controllers.getHelloController);
+exampleRoutes.post(
+  "/announcement",
+  dataIsValidMidd,
+  controllers.getHelloController
+);
