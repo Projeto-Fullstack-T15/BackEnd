4ef0b6964ae9eac0b0e7546f2d52bdbdb9a7dfde
Renan G. Dutra <r.granatodutra@gmail.com>
2023-08-19 11:56:19 -0300

    fix: identation and imports

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,28 +2,23 @@ import "dotenv/config";
 import "reflect-metadata";
 import { DataSource, DataSourceOptions } from "typeorm";
 import * as path from "path";
-import { Announcement } from "./modules/announcement/announcement";
-import { User } from "../src/entity/User";
-// import { User } from "./entity/User";
-// import { Account } from "./entity/Account";
-// import { Address } from "./entity/Address";
-//import { User } from "./entity/User";
+import { Announcement } from "./modules/announcement/announcement.entity";
+import { User } from "./modules/user/user.entity";
 
 const dataSourceConfig = (): DataSourceOptions => {
-  const migrationsPath: string = path.join(__dirname, "./migration/**.{ts,js}");
+	const migrationsPath: string = path.join(__dirname, "./migration/**.{ts,js}");
 
-  const { DB_TYPE, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } =
-    process.env;
-  const databaseURL = `${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
-  console.log(databaseURL);
-  return {
-    type: DB_TYPE as "postgres" | "mysql",
-    url: databaseURL,
-    synchronize: false,
-    logging: false,
-    entities: [Announcement, User],
-    migrations: [migrationsPath],
-  };
+	const { DB_TYPE, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+	const databaseURL = `${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+	return {
+		type: DB_TYPE as "postgres" | "mysql",
+		url: databaseURL,
+		synchronize: false,
+		logging: false,
+		entities: [Announcement, User],
+		migrations: [migrationsPath],
+	};
 };
 
 export const AppDataSource = new DataSource(dataSourceConfig());
